edaef5442d33be76a87e1acff5bbc90ea808b101
jpiaskowski <julia.piask@gmail.com>
2020-09-25 16:06:10 -0700

    usfws palettes added

---
+++ R/usfws.R
@@ -0,0 +1,17 @@
+#' U.S. Fish and Wildlife Services (usfws) palettes
+#'
+#' This selection of color palettes contains a list of palettes derived from this
+#' [Instagram post](https://www.instagram.com/p/CFP2H7JBSxR/?igshid=13sg433qx7ts6).
+#'
+#' The available palettes are:
+#' dreaming
+#' jojo
+#' beach
+#' waterfall
+#' sunset
+#'
+#' @export
+usfws <- list("sockeye_salmon" = c("#537B9E", "#C5C6CB", "#988421", "#82816C", "#282D33"),
+              "brook_trout" = c("#977559", "#5E6265", "#687897", "#CAB886", "#DE655D"),
+              "dolly_varden" = c("#97C0B0", "#899B71", "#979048", "#71767A", "#D07F6E"),
+              "florida_gar" = c("#050400", "#675B45", "#3E3526", "#988769", "#D2C398"))
